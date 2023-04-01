308226971a8c5217ecda10b41db2299b89ffce4c
m0i5 <111139099+m0i5@users.noreply.github.com>
2023-04-01 17:07:49 +0200

    Register / Login for Users on Lvl1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,17 +13,55 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/", function(req, res){
+app.get("/login", function(req, res){
     res.render("login");
 });
 
-app.get("/", function(req, res){
+app.get("/register", function(req, res){
     res.render("register");
 });
+
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+    .then(() => {res.render('secrets')})
+    .catch(() => {console.log("Error while saving data")})
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+    .then (function (foundUser) {
+        if (foundUser.password === password) {
+            res.render("secrets")
+        }
+        })
+        
+    .catch (function (err){console.log(err)})
+});
+
 
 
 
