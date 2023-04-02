20bff1ffc5e3ac83cdeae776a7908d6ab03a889e
m0i5 <111139099+m0i5@users.noreply.github.com>
2023-04-02 13:49:49 +0200

    Hashing with md5

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -28,7 +29,7 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 
@@ -54,17 +55,19 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
-    });
-
+        password: md5(req.body.password)
+    });   
     newUser.save()
     .then(() => {res.render('secrets')})
     .catch(() => {console.log("Error while saving data")})
 });
 
+
+
+
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
     .then (function (foundUser) {
